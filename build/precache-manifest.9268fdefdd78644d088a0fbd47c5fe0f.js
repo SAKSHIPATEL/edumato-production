a4f8a2eba591c761a7552a687f4a96233878a047
SAKSHIPATEL <patelsakshi121@gmail.com>
2020-11-07 09:03:37 +0530

    isomorphis app

---
+++ build/precache-manifest.9268fdefdd78644d088a0fbd47c5fe0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c173669059b2a08eb1c568cf09dc540f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33b91ddb993de4c1087c",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "c4aadb8f5038ddc5747f",
+    "url": "/static/css/main.5458b393.chunk.css"
+  },
+  {
+    "revision": "33b91ddb993de4c1087c",
+    "url": "/static/js/2.1d9570f6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1d9570f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4aadb8f5038ddc5747f",
+    "url": "/static/js/main.76b35361.chunk.js"
+  },
+  {
+    "revision": "0980db0307ffad65803b",
+    "url": "/static/js/runtime-main.df5858f9.js"
+  }
+]);
